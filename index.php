7cffa86d4c72a2a873a80cc9d2591482dec7ff1b
AimeeWMT <aimeesvn@gmail.com>
2013-07-17 14:05:32 +0800

    test
    test11111

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+//欢迎你使用Aimee Beta 0.0.1A测试版
+//如果发现BUG,请及时提交到bbs.aimees.net
+define('APP_DEBUG', true);
+define('APP_NAME', '');
+define('APP_PATH', './Source/');
+define("RUNTIME_PATH", "./Data/");
+define("CONF_PATH", RUNTIME_PATH . "Conf/");
+define("TMPL_PATH", "./Template/");
+require('./Source/ThinkPHP/ThinkPHP.php');
+?>
